c30084fdc7c66868b2a30895701f56d9771fff57
Eetu Mäkelä <eetu.makela@iki.fi>
2020-04-17 18:52:22 +0300

    update way to refer to credential files

--- s3-job/s3put.sh
+++ s3-job/s3put.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo Processing $@
-sort -u $@ | lrz | s3cmd -c /s3cfg put - s3://${BUCKET}/${DATA_FILE_PREFIX}_before_$(date +'%Y_%m_%d-%H_%M_%S').jsonl.lrz && rm $@
+sort -u $@ | lrz | s3cmd -c /credentials/s3cfg put - s3://${BUCKET}/${DATA_FILE_PREFIX}_before_$(date +'%Y_%m_%d-%H_%M_%S').jsonl.lrz && rm $@
 
