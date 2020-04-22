88d572990267a023135d7f8e9e1b6b50ed2ac482
Eetu Mäkelä <eetu.makela@iki.fi>
2020-04-22 18:05:54 +0300

    update merge job to produce also tweetids

--- s3-job/merge.py
+++ s3-job/merge.py
@@ -1,5 +1,7 @@
 import sys
+import re
 import heapq
+import gzip
 
 def decorated_file(f, key):
     """ Yields an easily sortable tuple. 
@@ -12,7 +14,7 @@ def standard_keyfunc(line):
     """
     return line
 
-def mergeSortedFiles(paths, keyfunc=standard_keyfunc):
+def mergeSortedFiles(paths, idspath, keyfunc=standard_keyfunc):
     """ Does the same thing SortedFileMerger class does. 
     """
     files = list(map(open, paths)) #open defaults to mode='r'
@@ -20,28 +22,33 @@ def mergeSortedFiles(paths, keyfunc=standard_keyfunc):
     dupLines = {f.name:0 for f in files}
     previous_file = None
     previous_comparable = None
-    for line in heapq.merge(*[decorated_file(f, keyfunc) for f in files]):
-        lines[line[2]] += 1
-        comparable = line[0]
-        if previous_comparable != comparable:
-            print(line[1], end = '')
-            previous_file = line[2]
-            previous_comparable = comparable
-        else:
-            if previous_file != None:
-                dupLines[previous_file] += 1
-                previous_file = None
-            dupLines[line[2]] += 1
+    with gzip.open(idspath,"wt") as idsf:
+        idsre = re.compile('"id_str": "(.*?)"')
+        for line in heapq.merge(*[decorated_file(f, keyfunc) for f in files]):
+            lines[line[2]] += 1
+            comparable = line[0]
+            if previous_comparable != comparable:
+                print(line[1], end = '')
+                m = idsre.search(line[1])
+                if m is not None:
+                    print(m[1],file=idsf)
+                previous_file = line[2]
+                previous_comparable = comparable
+            else:
+                if previous_file != None:
+                    dupLines[previous_file] += 1
+                    previous_file = None
+                dupLines[line[2]] += 1
     return (lines,dupLines)
 
 def main():
-    (lines,dupLines) = mergeSortedFiles(sys.argv[1:])
-    log = open("/data/merge.log","a")
-    for k in lines:
-        uniqLines = lines[k]-dupLines[k]
-        out = "{}: {:,}/{:,} ({:.2%})".format(k,uniqLines,lines[k],uniqLines/lines[k])
-        print(out,file=log)
-        print(out,file=sys.stderr)
+    (lines,dupLines) = mergeSortedFiles(sys.argv[1:-2],sys.argv[-2])
+    with open(sys.argv[-1],"a") as logf:
+        for k in lines:
+            uniqLines = lines[k]-dupLines[k]
+            out = "{}: {:,}/{:,} ({:.2%})".format(k,uniqLines,lines[k],uniqLines/lines[k])
+            print(out,file=logf)
+            print(out,file=sys.stderr)
 
 if __name__ == "__main__":
     main()
--- s3-job/s3put.sh
+++ s3-job/s3put.sh
@@ -4,5 +4,5 @@ for file in "$@"
 do
 sort -o $file $file
 done
-python /merge.py $@ | lrz | s3cmd -c /credentials/s3cfg put - s3://${BUCKET}/${DATA_FILE_PREFIX}_before_$(date +'%Y_%m_%d-%H_%M_%S').jsonl.lrz && rm $@
+python /merge.py $@ /data/tweetids.txt.gz /data/merge.log | lrz | s3cmd -c /credentials/s3cfg put - s3://${FULL_BUCKET}/${DATA_FILE_PREFIX}_before_$(date +'%Y_%m_%d-%H_%M_%S').jsonl.lrz && rm $@ && s3cmd -c /credentials/s3cfg put /data/tweetids.txt.gz s3://${ID_BUCKET}/${DATA_FILE_PREFIX}_before_$(date +'%Y_%m_%d-%H_%M_%S').txt.gz && rm /data/tweetids.txt.gz
 
